133ed78ff31f14c2d5105c535b9f8e42dc0e15a3
mlslsenarathna <senarat-ps19286@stu.kln.ac.lk>
2025-06-08 00:22:51 +0530

    Your descriptive commit message here

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 let number = Math.floor(Math.random() * 5) + 1;
-
+let clickcount=3;
 function guessNumber(){
+do{
 
+
+    clickcount--;
 let userNumber=document.getElementById("txtInput").value;
 
+if(clickcount>0){
+    if(userNumber>5 || userNumber<1){
+        document.getElementById('messageDisplay').innerText = "You  have Entered Wrong Number please Check .!";
+            
+    }else{
+            if(number==userNumber){
+                
 
-if(userNumber>5 || userNumber<1){
-    document.getElementById('messageDisplay').innerText = "You  have Entered Wrong Number please Check .!";
-        
+                    document.getElementById('messageDisplay').innerText = "You  have Win....!";
+                    
+
+
+            }else{
+                document.getElementById('messageDisplay').innerText = "Try again....!";
+                    
+                
+            }
+            console.log();
+    }
 }else{
-if(number==userNumber){
-    
 
-        document.getElementById('messageDisplay').innerText = "You  have Win....!";
-        
+    document.getElementById('messageDisplay').innerText = "You have use 3 chances...!";
 
+}
+}while(clickcount==0)
+}
 
-}else{
-    document.getElementById('messageDisplay').innerText = "Try again....!";
-        
-    
-}
-console.log();
-}
-}
